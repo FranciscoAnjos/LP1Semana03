4d51a31d886a274b32bf935b3dff77b00111ff79
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-06 19:39:11 +0000

    parse the matrix A and vector b (2x1) from the command-line arguments

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -11,6 +11,13 @@ namespace ArrayMul
                 Console.WriteLine("Please provide exactly 6 arguments.");
                 return;
             }
+            
+            float a11 = float.Parse(args[0]);
+            float a12 = float.Parse(args[1]);
+            float a21 = float.Parse(args[2]);
+            float a22 = float.Parse(args[3]);
+            float b1 = float.Parse(args[4]);
+            float b2 = float.Parse(args[5]);
         }
     }
 }
