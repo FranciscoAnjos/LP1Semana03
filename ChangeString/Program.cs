b9b36b7496a6be0eb3e733c4a58b5f579d602d2c
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-06 19:36:27 +0000

    ask for input

---
+++ ChangeString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string inputString = Console.ReadLine();
+
+            Console.Write("Enter a character to replace: ");
+            char charToReplace = Console.ReadKey().KeyChar;
+            Console.WriteLine(); 
+        }
+    }
+}
