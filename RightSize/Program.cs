8cca7c75494398dcaf005b86421a6a73eb5abe1d
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-06 19:34:30 +0000

    check if length greater equal to 8

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -6,12 +6,17 @@ namespace RightSize
     {
         static void Main(string[] args)
         {
-            
             foreach (string arg in args)
             {
                 // Check if the argument length is greater than 3
                 if (arg.Length > 3)
                 {
+                    if (arg.Length >= 8)
+                    {
+                        Console.WriteLine("[EARLY STOP]");
+                        return;
+                    }
+                    
                     Console.WriteLine(arg);
                 }
             }
