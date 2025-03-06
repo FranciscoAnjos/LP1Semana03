1bb873ce5b6e2ab1bc874733c83bc23b7de1fe1b
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-06 19:38:39 +0000

    first we check if args are the correct number

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArrayMul
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 6)
+            {
+                Console.WriteLine("Please provide exactly 6 arguments.");
+                return;
+            }
+        }
+    }
+}
