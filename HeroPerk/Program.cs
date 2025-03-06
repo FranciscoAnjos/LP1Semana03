85a627bc2afa9e8f876cba46aef11b26cf66f698
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-06 19:50:08 +0000

    write perks back to console

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -16,10 +16,10 @@ namespace HeroPerk
                     case 'w': 
                         playerPerks ^= Perks.WarpShift; 
                         break; 
-                    case 'a': 
+                    case 's': 
                         playerPerks ^= Perks.Stealth; 
                         break;
-                    case 's': 
+                    case 'a': 
                         playerPerks ^= Perks.AutoHeal; 
                         break;
                     case 'd': 
@@ -29,6 +29,39 @@ namespace HeroPerk
                         Console.WriteLine("!Unknown perk!");
                         return;
                 }
+            }
+            
+            if (playerPerks == Perks.None)
+            {
+                Console.WriteLine("!No perks at all!");
+                Console.WriteLine("!Not gonna make it!");
+            }
+            else
+            {
+                Console.WriteLine(GetPerksString(playerPerks));
+
+                if ((playerPerks & Perks.Stealth) == Perks.Stealth && 
+                    (playerPerks & Perks.DoubleJump) == Perks.DoubleJump)
+                {
+                    Console.WriteLine("!Silent jumper!");
+                }
+
+                if ((playerPerks & Perks.AutoHeal) == 0)
+                {
+                    Console.WriteLine("!Not gonna make it!");
+                }
+            }
+            
+            static string GetPerksString(Perks perks)
+            {
+                var perkList = new System.Collections.Generic.List<string>();
+
+                if ((perks & Perks.WarpShift) != 0) perkList.Add("WarpShift");
+                if ((perks & Perks.Stealth) != 0) perkList.Add("Stealth");
+                if ((perks & Perks.AutoHeal) != 0) perkList.Add("AutoHeal");
+                if ((perks & Perks.DoubleJump) != 0) perkList.Add("DoubleJump");
+
+                return string.Join(", ", perkList);
             }
         }
     }
