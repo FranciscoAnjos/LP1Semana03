a9795edbe78648d8c6b6eb9156b216eb3c12139d
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-06 19:45:18 +0000

    init input and playerperks

---
+++ HeroPerk/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input = args[0];
+            Perks playerPerks = Perks.None;
+        }
+    }
+    
+    [Flags]
+    enum Perks
+    {
+        None = 0,
+        WarpShift = 1 << 0,  // 1
+        Stealth = 1 << 1,    // 2
+        AutoHeal = 1 << 2,   // 4
+        DoubleJump = 1 << 3  // 8
+    }
+}
