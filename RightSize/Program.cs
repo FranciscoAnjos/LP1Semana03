6086ad4395659b10e503533be889a44282117d34
FranciscoAnjos <fcariaanjos@gmail.com>
2025-03-06 19:34:03 +0000

    cheeck if arg length is greater than 3

---
+++ RightSize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            foreach (string arg in args)
+            {
+                // Check if the argument length is greater than 3
+                if (arg.Length > 3)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+    }
+
+}
